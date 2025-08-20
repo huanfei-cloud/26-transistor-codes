fbe6642e929d5dd842adb011f2efc8c08076f8e0
huanfei-cloud <1542701750@qq.com>
2025-08-20 15:40:46 +0800

    sentry代码微改

--- Sentry_Board2_C_26/APPs/APPs.c/Steer_Omni_Chassis.c
+++ Sentry_Board2_C_26/APPs/APPs.c/Steer_Omni_Chassis.c
@@ -52,11 +52,11 @@ void Chassis_Init(void)
 		Position_PIDInit(&(M3508_Array[2].v_pid_object),10.0f, 0.22f, 0,0,800,30000,6000);
 		Position_PIDInit(&(M3508_Array[3].v_pid_object),10.0f, 0.22f, 0,0,800,30000,6000);
     //转向电机速度环初始化
-  	Position_PIDInit(&(M6020s_Chassis1.v_pid_object),1,0.1,1,0,7000,30000,6000);
-    Position_PIDInit(&(M6020s_Chassis2.v_pid_object),1,0.1,1,0,7000,30000,6000);
+  	Position_PIDInit(&(M6020s_Chassis1.v_pid_object),250,0.1,150,0,7000,30000,6000);
+    Position_PIDInit(&(M6020s_Chassis2.v_pid_object),250,0.1,150,0,7000,30000,6000);
     //转向电机位置环初始化
-		Position_PIDInit(&(M6020s_Chassis1.l_pid_object),0.032f, 0.00001f, 0.05, 0, 30000, 10000 ,10000);
-		Position_PIDInit(&(M6020s_Chassis2.l_pid_object),0.032f, 0.00001f, 0.05, 0, 30000, 10000 ,10000);
+		Position_PIDInit(&(M6020s_Chassis1.l_pid_object),2.032f, 0.00001f, 0.05, 0, 30000, 10000 ,10000);
+		Position_PIDInit(&(M6020s_Chassis2.l_pid_object),2.032f, 0.00001f, 0.05, 0, 30000, 10000 ,10000);
 
     M3508_Array[0].targetSpeed = 0.0f;
     M3508_Array[1].targetSpeed = 0.0f;
